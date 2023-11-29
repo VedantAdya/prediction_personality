a51bbf7292e33857b9835bd2060849a6342b09aa
VedantAdya <102545015+VedantAdya@users.noreply.github.com>
2023-11-29 14:01:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for, session, flash
+from flask import Flask, render_template, request, redirect, session, flash
 import os
 import numpy as np
 import sqlite3
